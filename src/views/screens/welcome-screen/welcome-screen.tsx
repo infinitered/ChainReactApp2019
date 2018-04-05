a5e1705e331b8f06fca7bd54f19fbd4384cc315d
Kevin VanGelder <kevin@infinite.red>
2018-04-05 10:11:33 -0700

    implement feedback

--- src/views/screens/welcome-screen/welcome-screen.tsx
+++ src/views/screens/welcome-screen/welcome-screen.tsx
@@ -1,39 +1,38 @@
 import * as React from "react"
+import { ViewStyle, TextStyle } from "react-native"
 import { Text } from "../../shared/text"
 import { Button } from "../../shared/button"
 import { NavigationScreenProps } from "react-navigation"
 import { Screen } from "../../shared/screen"
 import { Footer } from "../../shared/footer"
 import { palette } from "../../../theme/palette"
+import { spacing } from "../../../theme/spacing"
 const backgroundImage = require("./bg.welcome.png")
 
 export interface WelcomeScreenProps extends NavigationScreenProps<{}> {}
+
+const ROOT: ViewStyle = { justifyContent: "flex-end" }
+const HEADER1: TextStyle = { paddingHorizontal: spacing.large, color: palette.white }
+const HEADER2: TextStyle = {
+  marginBottom: 38,
+  paddingHorizontal: spacing.large,
+  color: palette.offWhite,
+}
+const SUBHEADER: TextStyle = {
+  marginBottom: 290,
+  paddingHorizontal: spacing.large,
+  color: palette.offWhite,
+}
 
 export class WelcomeScreen extends React.Component<WelcomeScreenProps, {}> {
   nextScreen = () => this.props.navigation.navigate("info")
 
   render() {
     return (
-      <Screen
-        preset="fixed"
-        style={{ justifyContent: "flex-end" }}
-        backgroundImage={backgroundImage}
-      >
-        <Text
-          preset="header"
-          tx="welcomeScreen.header1"
-          style={{ paddingHorizontal: 20, color: palette.white }}
-        />
-        <Text
-          preset="header"
-          tx="welcomeScreen.header2"
-          style={{ marginBottom: 38, paddingHorizontal: 20, color: palette.offWhite }}
-        />
-        <Text
-          preset="subheader"
-          tx="welcomeScreen.subtitle"
-          style={{ marginBottom: 290, paddingHorizontal: 20, color: palette.offWhite }}
-        />
+      <Screen preset="fixed" style={ROOT} backgroundImage={backgroundImage}>
+        <Text preset="header" tx="welcomeScreen.header1" style={HEADER1} />
+        <Text preset="header" tx="welcomeScreen.header2" style={HEADER2} />
+        <Text preset="subheader" tx="welcomeScreen.subtitle" style={SUBHEADER} />
         <Footer>
           <Button
             tx="welcomeScreen.nextScreenButton"
