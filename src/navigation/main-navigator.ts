e1a4b700d7389b491ee3bdb67030d1debe179c42
Kevin VanGelder <kevin@infinite.red>
2018-04-06 16:33:53 -0700

    add talk details screen

--- src/navigation/main-navigator.ts
+++ src/navigation/main-navigator.ts
@@ -2,6 +2,7 @@ import { StackNavigator, TabNavigator } from "react-navigation"
 import { ScheduleScreen } from "../views/screens/schedule-screen"
 import { VenueScreen } from "../views/screens/venue-screen"
 import { InfoScreen } from "../views/screens/info-screen"
+import { TalkDetailsScreen } from "../views/screens/talk-details"
 import { TabBar } from "../views/shared/tab-bar"
 
 export const MainNavigator = StackNavigator(
@@ -11,10 +12,12 @@ export const MainNavigator = StackNavigator(
         {
           schedule: StackNavigator(
             {
-              screen: ScheduleScreen,
+              schedule: ScheduleScreen,
+              talkDetails: TalkDetailsScreen,
             },
             {
               headerMode: "none",
+              initialRouteName: "schedule",
             },
           ),
           venue: { screen: VenueScreen },
