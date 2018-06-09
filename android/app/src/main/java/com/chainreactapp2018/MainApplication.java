1dc801921d75e0d43815f8f195e0ce7dc2528efb
Andy Chu <andychu@viromedia.com>
2018-06-09 13:22:02 -0700

    Changes required to build for Android/iOS devices
    - [Android] followed steps for mapbox setup from this link:
      https://github.com/mapbox/react-native-mapbox-gl/blob/master/android/install.md
    - [iOS] set the device type to universal (to support iPad)
    - [iOS] set "Enable Bitcode" to "No"

--- android/app/src/main/java/com/chainreactapp2018/MainApplication.java
+++ android/app/src/main/java/com/chainreactapp2018/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.mapbox.rctmgl.RCTMGLPackage;
 
 import java.util.Arrays;
 import java.util.List;
