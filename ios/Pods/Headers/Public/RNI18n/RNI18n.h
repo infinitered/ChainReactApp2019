a837db57efe0bf58feedd1945cdb2062b1f4558d
Ryan Linton <ryan@infinite.red>
2018-06-12 22:19:58 +0700

    Get LaunchScreen working on iOS

---
+++ ios/Pods/Headers/Public/RNI18n/RNI18n.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-i18n/ios/RNI18n.h
---
+++ ios/Pods/Target Support Files/RNI18n/RNI18n-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RNI18n : NSObject
+@end
+@implementation PodsDummy_RNI18n
+@end
