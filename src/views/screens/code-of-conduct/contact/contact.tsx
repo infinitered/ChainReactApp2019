ba22de599176d6f849f534a06a32bddd260492fb
Robin Heinze <robin.m.heinze@gmail.com>
2018-06-25 17:00:02 -0700

    Add call/text option to CoC screen

--- src/views/screens/code-of-conduct/contact/contact.tsx
+++ src/views/screens/code-of-conduct/contact/contact.tsx
@@ -1,12 +1,14 @@
 import * as React from "react"
-import { View, ViewStyle, TextStyle, TouchableOpacity, Linking } from "react-native"
+import { View, ViewStyle, TextStyle, TouchableOpacity, Linking, Alert } from "react-native"
 import { spacing } from "../../../../theme/spacing"
 import { SocialButton } from "../../../shared/social-button"
 import { Text } from "../../../shared/text"
+import { translate } from "../../../../i18n"
 
 interface ContactProps {
   email: string
   twitter: string
+  phoneNumber: string
 }
 const ROOT: ViewStyle = {
   padding: spacing.large,
@@ -22,8 +24,30 @@ const TEXT: TextStyle = {
 }
 
 export class Contact extends React.Component<ContactProps, {}> {
+  openCallTextAlertDialog = () => {
+    const { phoneNumber } = this.props
+    const callLink = `tel:${phoneNumber}`
+    const smsLink = `sms:${phoneNumber}`
+    const title = translate("codeOfConductScreen.contact.alertTitle")
+    const message = translate("codeOfConductScreen.contact.alertBody")
+    const text = translate("codeOfConductScreen.contact.text")
+    const call = translate("codeOfConductScreen.contact.call")
+    const cancel = translate("common.cancel")
+
+    Alert.alert(
+      title,
+      message,
+      [
+        { text: call, onPress: () => Linking.openURL(callLink) },
+        { text: text, onPress: () => Linking.openURL(smsLink) },
+        { text: cancel, style: "cancel" },
+      ],
+      { cancelable: true },
+    )
+  }
+
   render() {
-    const { email, twitter } = this.props
+    const { email, twitter, phoneNumber } = this.props
     const twitterText = `@${twitter}`
     const twitterLink = `https://twitter.com/${twitter}`
     const emailLink = `mailto:${email}`
@@ -37,6 +61,10 @@ export class Contact extends React.Component<ContactProps, {}> {
         <TouchableOpacity onPress={() => Linking.openURL(twitterLink)} style={ROW}>
           <SocialButton link={twitterLink} preset="twitter" />
           <Text style={TEXT} text={twitterText.toUpperCase()} preset="body" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.openCallTextAlertDialog} style={ROW}>
+          <SocialButton link={""} preset="phone" />
+          <Text style={TEXT} text={phoneNumber} preset="body" />
         </TouchableOpacity>
       </View>
     )
