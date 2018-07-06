0a732edebe3264589786d268f08ea5a8bb07e620
Gant <GantMan@gmail.com>
2018-07-06 17:34:05 -0500

    grow instead of fade

--- src/views/screens/schedule-screen/schedule-cell/schedule-cell.tsx
+++ src/views/screens/schedule-screen/schedule-cell/schedule-cell.tsx
@@ -1,5 +1,13 @@
 import * as React from "react"
-import { View, ViewStyle, Image, ImageStyle, TextStyle, TouchableOpacity } from "react-native"
+import {
+  View,
+  ViewStyle,
+  Image,
+  ImageStyle,
+  TextStyle,
+  TouchableWithoutFeedback,
+  Animated,
+} from "react-native"
 import { ScheduleCellPresets } from "./schedule-cell.presets"
 import { ScheduleCellProps } from "./schedule-cell.props"
 import { Text } from "../../../shared/text"
@@ -7,36 +15,66 @@ import { palette } from "../../../../theme/palette"
 import { format } from "date-fns"
 
 export class ScheduleCell extends React.Component<ScheduleCellProps, {}> {
+  state = { animatedSize: new Animated.Value(1) }
+
+  handlePressIn = () => {
+    Animated.spring(this.state.animatedSize, {
+      toValue: 1.05,
+      useNativeDriver: true,
+    }).start()
+  }
+
+  handlePressOut = () => {
+    Animated.spring(this.state.animatedSize, {
+      toValue: 1,
+      friction: 5,
+      useNativeDriver: true,
+    }).start()
+  }
+
   render() {
     const { preset, index, talk, onPress, noTime } = this.props
     const style: any = ScheduleCellPresets[preset] || ScheduleCellPresets.default
     const isOdd = index % 2 === 0 // index starts at 0
     if (!talk) return null
     return (
-      <TouchableOpacity
-        style={[style.root as ViewStyle, isOdd && { backgroundColor: palette.portGoreLighter }]}
+      <TouchableWithoutFeedback
+        onPressIn={this.handlePressIn}
+        onPressOut={this.handlePressOut}
         onPress={() => onPress(this.props.talk)}
       >
-        {noTime ? this.renderTopBorder() : this.renderTime()}
-        <View style={style.contentWrapper as ViewStyle}>
-          <View style={style.imageWrapper as ViewStyle}>{this.renderImage()}</View>
-          <View style={style.content as ViewStyle}>
-            <Text preset="subheader" text={talk.title} style={style.title as TextStyle} />
-            {talk.speakers &&
-              talk.speakers.length > 0 &&
-              talk.speakers[0].name && (
+        <Animated.View
+          style={[
+            style.root as ViewStyle,
+            { transform: [{ scale: this.state.animatedSize }] },
+            isOdd && { backgroundColor: palette.portGoreLighter },
+          ]}
+        >
+          {noTime ? this.renderTopBorder() : this.renderTime()}
+          <View style={style.contentWrapper as ViewStyle}>
+            <View style={style.imageWrapper as ViewStyle}>{this.renderImage()}</View>
+            <View style={style.content as ViewStyle}>
+              <Text preset="subheader" text={talk.title} style={style.title as TextStyle} />
+              {talk.speakers &&
+                talk.speakers.length > 0 &&
+                talk.speakers[0].name && (
+                  <Text
+                    preset="subheader"
+                    text={talk.speakers[0].name}
+                    style={style.speaker as TextStyle}
+                  />
+                )}
+              {preset === "afterparty" && (
                 <Text
                   preset="subheader"
-                  text={talk.speakers[0].name}
+                  text={talk.description}
                   style={style.speaker as TextStyle}
                 />
               )}
-            {preset === "afterparty" && (
-              <Text preset="subheader" text={talk.description} style={style.speaker as TextStyle} />
-            )}
+            </View>
           </View>
-        </View>
-      </TouchableOpacity>
+        </Animated.View>
+      </TouchableWithoutFeedback>
     )
   }
 
