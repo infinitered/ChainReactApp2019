c95b5420787f4c9930a842634e986ace5bfc1b21
Robin Heinze <robin.m.heinze@gmail.com>
2019-05-27 11:26:31 -0700

    Get CI setup with .env

--- app/aws-exports.js
+++ app/aws-exports.js
@@ -7,7 +7,7 @@ const awsmobile = {
     "https://najx2aa76zb55jeexyyc43nuyy.appsync-api.us-east-2.amazonaws.com/graphql",
   aws_appsync_region: "us-east-2",
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: AWS_APPSYNC_API_KEY,
+  aws_appsync_apiKey: AWS_APPSYNC_API_KEY || process.env.AWS_APPSYNC_API_KEY,
 }
 
 export default awsmobile
