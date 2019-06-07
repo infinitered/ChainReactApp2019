223ddbbfd008c43e64a458365345e4efb0954c7b
Robin Heinze <robin.m.heinze@gmail.com>
2019-06-07 14:04:26 -0700

    Switch to react-native-localize from react-native-i18n

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -1,7 +1,19 @@
-import I18n from "react-native-i18n"
+import I18n from "i18n-js"
+import * as RNLocalize from "react-native-localize"
 
-const en = require("./en")
-const ja = require("./ja")
+const en = require("./locales/en.json")
+const ja = require("./locales/ja.json")
+
+const locales = RNLocalize.getLocales()
+
+if (Array.isArray(locales)) {
+  I18n.locale = locales[0].languageTag
+}
 
 I18n.fallbacks = true
-I18n.translations = { en, ja }
+I18n.translations = {
+  en,
+  ja,
+}
+
+export default I18n
