80c4e42b8965f452ed280e0eadcf7fe0cb852989
Morgan <morgan.laco@gmail.com>
2019-06-24 18:51:55 -0400

    Make additional sponsor name text same size (#156)
    * Additional sponsor images without text
    
    * Replace Additional_G2i images
    
    * Sponsor subtitles
    
    * Restore bronze G2i images
    
    * Green subtitles
    
    * Style additional sponsor subtitles
    
    * Conditionally use subtitle styles
    
    * Update snapshot
    
    * Space in "AFTER PARTY"
    
    * Update snapshot

--- app/components/sponsor-logo/sponsor-logo.tsx
+++ app/components/sponsor-logo/sponsor-logo.tsx
@@ -1,13 +1,40 @@
 import * as React from "react"
-import { Image } from "react-native"
+import { Image, View, ViewStyle, TextStyle } from "react-native"
+import { Text } from "../text"
 import { SponsorLogoProps } from "./sponsor-logo.props"
 import { sponsorLogoSizePresets, sponsors } from "./sponsor-logo.presets"
+import { color, spacing } from "../../theme"
+
+const ROOT: ViewStyle = {
+  flexDirection: "column",
+  width: "50%",
+  alignItems: "center",
+}
+
+const SUBTITLE: TextStyle = {
+  fontSize: 12,
+  fontWeight: "600",
+  letterSpacing: 3.0,
+  color: color.palette.shamrock,
+}
 
 export class SponsorLogo extends React.Component<SponsorLogoProps, {}> {
   render() {
-    const { size, sponsor, style } = this.props
-    return (
-      <Image source={sponsors[sponsor]} style={{ ...sponsorLogoSizePresets[size], ...style }} />
-    )
+    const { size, sponsor, style, subtitle } = this.props
+    if (!subtitle) {
+      return (
+        <Image source={sponsors[sponsor]} style={{ ...sponsorLogoSizePresets[size], ...style }} />
+      )
+    } else {
+      return (
+        <View style={ROOT}>
+          <Image
+            source={sponsors[sponsor]}
+            style={{ ...sponsorLogoSizePresets[size], ...style, marginBottom: 0 }}
+          />
+          {subtitle && <Text style={SUBTITLE} text={subtitle} />}
+        </View>
+      )
+    }
   }
 }
