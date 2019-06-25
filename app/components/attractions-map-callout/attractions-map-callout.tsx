d01c47ceff4f29c24d5c1e007103a355f0073abf
Robin Heinze <robin.m.heinze@gmail.com>
2019-06-25 14:32:39 -0700

    Fix zIndex of Map Callout

--- app/components/attractions-map-callout/attractions-map-callout.tsx
+++ app/components/attractions-map-callout/attractions-map-callout.tsx
@@ -23,7 +23,7 @@ export interface AttractionsMapCalloutProps {
 const CALLOUT_CONTAINER: ViewStyle = {
   alignItems: "center",
   justifyContent: "center",
-  overflow: "hidden",
+  zIndex: 1,
 }
 
 const CALLOUT_CONTENT_CONTAINER: ViewStyle = {
